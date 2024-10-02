0e48dfdbce2890f2bb69db92e1784b96b47b7ee2
Pierre GUEIT-DESSUS <pierregueitdessus@gmail.com>
2024-10-02 11:45:00 +0200

    Atomic templating

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home')]
+    #[Route('/')]
     public function index(): Response
     {
         return $this->render(view: 'home/index.html.twig');
